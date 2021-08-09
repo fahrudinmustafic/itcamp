f8d61f19727bb78c04e0d20f35f02e9eac57beb5
Fahrudin Mustafić <86748878+fahrudinmustafic@users.noreply.github.com>
2021-08-09 13:05:19 +0200

    Add files via upload

---
+++ javascript/vezba5/switch.js
@@ -0,0 +1,33 @@
+var day='Monday';
+
+
+switch (day){
+
+  case "Monday":
+  console.log('Ponedeljak')
+  break;
+
+  case "Tuestday":
+  console.log('Utorak')
+  break;
+
+  case "Wednesday":
+  console.log('Sreda')
+  break;
+
+  case "Thursday":
+  console.log('Cetvrtak')
+  break;
+
+  case "Frayday":
+  console.log('petak')
+  break;
+
+  case "Saturday":
+  console.log('Subota')
+  break;
+
+  case "Sunday":
+  console.log('Nedelja')
+  break;
+}
