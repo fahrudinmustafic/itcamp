fafb2965745b44abe1abee87222607fdc568cdbe
Fahrudin Mustafić <86748878+fahrudinmustafic@users.noreply.github.com>
2021-08-09 13:04:58 +0200

    Add files via upload

---
+++ javascript/vezba4/if.js
@@ -0,0 +1,32 @@
+
+var number=80;
+
+
+if (number <= 51) {
+
+console.log(5)
+}
+else if(number <=61){
+
+  console.log(6)
+}
+
+else if(number <=71){
+
+  console.log(7)
+
+}
+  else if(number <=81){
+
+  console.log(8)
+}
+  else if(number <=91){
+
+  console.log(9)
+
+}
+else if(number >91){
+
+  console.log(10)
+
+}
