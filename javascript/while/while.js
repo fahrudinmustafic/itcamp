e4b0d04bb0b26ee9b8399accf2d31d1022936745
Fahrudin Mustafić <86748878+fahrudinmustafic@users.noreply.github.com>
2021-08-11 00:30:48 +0200

    Update while.js

--- javascript/while/while.js
+++ javascript/while/while.js
@@ -1,8 +1,7 @@
 // brojanje od 0 do 100
-var broj = 0;
-var zbir = 0;
-while ( broj < 100 ) {
-	zbir = zbir + broj;
-	broj = broj + 1;
-  console.log(broj);
-} 
+var a=0;
+while( a<100){
+a=a+1;
+console.log(a);
+
+}
